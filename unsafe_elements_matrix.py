eb96fd5aebf0e3aabd6e8aa0cfb1092cd5a2f00a
VaidyaAnuja <65403961+VaidyaAnuja@users.noreply.github.com>
2022-08-07 16:48:13 +0530

    Update unsafe_elements_matrix.py

--- unsafe_elements_matrix.py
+++ unsafe_elements_matrix.py
@@ -3,23 +3,20 @@
 #https://www.hackerearth.com/practice/data-structures/arrays/multi-dimensional/practice-problems/algorithm/kshitiz-and-matrix-7ddc9719/
 
 li = []
-no = int(input("Enter the number of cases"))
-for i in range(no):
-    nm = input("Enter N and M")
-    l = list(nm)
-    l.remove(" ")
-    for j in range(int(l[0])):
-        ll = [int(ll) for ll in input("Enter multiple values: ").split()]
-        for k in range(0, len(ll) - int(l[1])):
+T = int(input())
+for i in range(T):
+    N, M = input().split()
+    for j in range(int(N)):
+        ll = [int(ll) for ll in input().split()]
+        for k in range(0, len(ll) - int(M)):
             ll.pop()
         li.append(ll)
-    print(li)
 
     small = li[0][0]
     high = li[0][0]
 
-    for i in range(int(l[0])):
-        for j in range(int(l[1])):
+    for i in range(int(N)):
+        for j in range(int(M)):
             if li[i][j]<small:
                 small = li[i][j]
             if li[i][j]>high:
@@ -28,13 +25,13 @@ for i in range(no):
     r_counter = []
     c_counter = []
 
-    for i in range(int(l[0])):
-        for j in range(int(l[1])):
+    for i in range(int(N)):
+        for j in range(int(M)):
             if li[i][j] == small or li[i][j] == high:
                 if i not in r_counter:
                     r_counter.append(i)
                 if j not in c_counter:
                     c_counter.append(j)
-                
-    out = (int(l[0])-len(r_counter)) * (int(l[1])-len(c_counter))
+    out = (int(N)-len(r_counter)) * (int(M)-len(c_counter))
     print(out)
+
